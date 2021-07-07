072944c1a9d69ff4ea1ea1fddec1a641a12e0fd7
smathur910 <smathur910@gmail.com>
2021-07-07 09:45:44 +0530

    😀 Initial step react

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import "./Navbar.css"
+
+const Navbar = () => {
+    return (
+
+        <div id="header">
+            <div className="header-container">
+                <div className="logo">
+                    <img src="/images/anish.png" alt=""></img>
+                </div>
+                <div className="logo-title">
+                    <h1>imanish</h1>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
